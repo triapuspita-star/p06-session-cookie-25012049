2c4e97c0978c65e894c4a6167a518ec7ab571fd7
triapuspita-star <puspitatria31@gmail.com>
2026-09-17 14:15:51 +0700

    chore: hapus checkout.php tidak sesuai modul praktik B

--- checkout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-echo "Checkout berhasil! Cookie Username: " . ($_COOKIE['username'] ?? 'tidak ada');
-$_SESSION['cart'] = [];
-?>
-<br><br><a href="index.php">Belanja Lagi</a>
